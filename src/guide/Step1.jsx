37ada11f207cb3415f8769404cfd05724f2806cf
army8735 <army8735@qq.com>
2017-04-20 22:56:31 +0800

    step1

--- src/guide/Step1.jsx
+++ src/guide/Step1.jsx
@@ -12,11 +12,11 @@ class Step1 extends migi.Component {
       <img class="logo" src="step1.jpg"/>
       <h2>欢迎来到转圈</h2>
       <p class="qname">我是圈娘，请问该怎么称呼你呢？</p>
-      <input type="text" class="iname"/>
+      <input type="text" class="iname" placeholder="字数请超过4个" maxLength="16"/>
       <p class="qsex">请问是汉子还是妹子呢？</p>
       <ul class="sex">
-        <li class="male">汉子</li>
-        <li class="female">妹子</li>
+        <li class="male cur"><span>汉子</span></li>
+        <li class="female"><span>妹子</span></li>
       </ul>
     </div>;
   }
