750f22e3329652c02e5b8752e90141b42f522411
army8735 <army8735@qq.com>
2017-05-01 17:13:05 +0800

    toast/loading

--- src/common/bridge.js
+++ src/common/bridge.js
@@ -2,6 +2,14 @@
  * Created by army on 2017/3/19.
  */
 
+var toString = {}.toString;
+function isType(type) {
+  return function(obj) {
+    return toString.call(obj) == '[object ' + type + ']';
+  }
+}
+var isString = isType('String');
+
 let bridge = {
   ready: function(cb) {
     cb = cb || function() {};
@@ -31,6 +39,30 @@ let bridge = {
   },
   popWindow: function(data) {
     JSBridge.call('popWindow', JSON.stringify(data));
+  },
+  toast: function(s) {
+    JSBridge.call('toast', s);
+  },
+  showLoading: function(s) {
+    if(isString(s)) {
+      s = {
+        title: '',
+        message: s,
+        cancelable: true
+      };
+    }
+    else {
+      s = s || {};
+      s.title = s.title || '';
+      s.message = s.message || '加载中...';
+      if(s.cancelable === undefined) {
+        s.cancelable = true;
+      }
+    }
+    JSBridge.call('showLoading', s);
+  },
+  hideLoading: function() {
+    JSBridge.call('hideLoading');
   }
 };
 
