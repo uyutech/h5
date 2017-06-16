f3131bf8e2057800f4f84b692c3046a3df834545
army8735 <army8735@qq.com>
2017-06-16 23:44:53 +0800

    author

---
+++ src/author/Nav.jsx
@@ -0,0 +1,45 @@
+/**
+ * Created by army on 2017/6/16.
+ */
+ 
+class Nav extends migi.Component {
+  constructor(...data) {
+    super(...data);
+  }
+  render() {
+    return <div class="nav">
+      <b class="back"/>
+      <b class="play"/>
+      <div class="profile">
+        <div class="pic">
+          <img src="http://tva3.sinaimg.cn/crop.0.0.328.328.50/6924ccf1gw1f889w9il5pj209709e0tx.jpg"/>
+          <b class="v"/>
+        </div>
+        <div class="txt">
+          <div class="n">
+            <h3>司夏</h3>
+            <span>策</span>
+            <span>歌</span>
+          </div>
+          <p class="intro">别名：揩油哥</p>
+          <div class="o">
+            <div class="fans">
+              <strong>6.6w</strong>
+              <span>粉丝</span>
+            </div>
+            <div class="hot">
+              <div class="line">
+                <b class="progress"/>
+                <b class="point"/>
+              </div>
+              <span>热度</span>
+            </div>
+            <a href="#">应援</a>
+          </div>
+        </div>
+      </div>
+    </div>;
+  }
+}
+
+export default Nav;
---
+++ src/author/index.jsx
@@ -0,0 +1,15 @@
+/**
+ * Created by army on 2017/6/16.
+ */
+ 
+import './author.html';
+import './index.less';
+
+import Nav from './Nav.jsx';
+
+jsBridge.ready(function() {
+  migi.render(
+    <Nav/>,
+    document.body
+  );
+});
