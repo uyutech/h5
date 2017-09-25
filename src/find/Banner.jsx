e7de329ceab77449dc5b4bcceff7ba36aaf2a5fd
army8735 <army8735@qq.com>
2017-09-25 21:19:17 +0800

    update

--- src/find/Banner.jsx
+++ src/find/Banner.jsx
@@ -6,8 +6,14 @@ class Banner extends migi.Component {
   constructor(...data) {
     super(...data);
   }
+  click(e, vd) {
+    e.preventDefault();
+    jsBridge.pushWindow(vd.props.href);
+  }
   render() {
-    return <div class="banner"><pre>转圈一周精选<br/>Vol 01</pre></div>;
+    return <div class="banner">
+      <a href="works.html?id=2015000000000001" onClick={ this.click }><img src="http://zhuanquan.xyz/pic/47d15679d3e6883acacdbcd85f2ebe85.jpg"/></a>
+    </div>;
   }
 }
 
