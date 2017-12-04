7522cf18053fc0811d2c8a45db2293ee32df8ac5
army8735 <army8735@qq.com>
2017-12-04 20:25:36 +0800

    author

--- src/author/Author.jsx
+++ src/author/Author.jsx
@@ -6,6 +6,10 @@
 
 import Nav from './Nav.jsx';
 import Home from './Home.jsx';
+import MAList from './MAList.jsx';
+import Comments from './Comments.jsx';
+import SubCmt from '../component/subcmt/SubCmt.jsx';
+import WorksTypeEnum from "../works/WorksTypeEnum";
 
 class Author extends migi.Component {
   constructor(...data) {
@@ -13,6 +17,8 @@ class Author extends migi.Component {
   }
   @bind hasData
   @bind authorID
+  @bind rid
+  @bind cid
   setData(authorID, data) {
     let self = this;
     self.authorID = authorID;
@@ -23,6 +29,92 @@ class Author extends migi.Component {
     self.hotPlayList = data.hotPlayList;
 
     self.hasData = true;
+
+    let subCmt = self.ref.subCmt;
+    subCmt.on('click', function() {
+      if(subCmt.to) {
+        jsBridge.pushWindow('/subcomment.html?type=2&id='
+          + self.authorID + '&cid=' + self.cid + '&rid=' + self.rid, {
+          title: '评论',
+        });
+      }
+      else {
+        jsBridge.pushWindow('/subcomment.html?type=2&id=' + self.authorID, {
+          title: '评论',
+        });
+      }
+    });
+  }
+  clickType(e, vd ,tvd) {
+    let $li = $(tvd.element);
+    if($li.hasClass('cur')) {
+      return;
+    }
+    $(vd.element).find('.cur').removeClass('cur');
+    $li.addClass('cur');
+    let self = this;
+    let home = self.ref.home;
+    let maList = self.ref.maList;
+    let comments = self.ref.comments;
+    home && home.hide();
+    maList && maList.hide();
+    comments && comments.hide();
+    let rel = tvd.props.rel;
+    switch(rel) {
+      case 'home':
+        home.show();
+        break;
+      case 'ma':
+        if(!maList) {
+          self.ref.maList = maList = migi.render(
+            <MAList ref="maList" authorID={ self.authorID }
+                    dataList={ self.hotPlayList }/>);
+          maList.after(home.element
+          );
+        }
+        maList && maList.show();
+        break;
+      case 'comments':
+        self.addComment();
+        comments && comments.show();
+        break;
+    }
+  }
+  addComment() {
+    let self = this;
+    if(self.ref.comments) {
+      return;
+    }
+    let comments = self.ref.comments = migi.render(
+      <Comments ref="comments"
+                isLogin={ $.cookie('isLogin') === 'true' }
+                authorID={ self.authorID }
+                commentData={ self.commentData }/>
+    );
+    self.ref.comments.after(self.ref.home.element);
+
+    let comment = comments.ref.comment;
+    let subCmt = self.ref.subCmt;
+    if(self.worksType === WorksTypeEnum.TYPE.originMusic) {
+      let media = self.ref.media;
+      media.on('switchTo', function(data) {
+        comments.workID = data.ItemID;
+      });
+    }
+    comment.on('chooseSubComment', function(rid, cid, name, n) {
+      subCmt.to = name;
+      self.rid = rid;
+      self.cid = cid;
+      if(!n || n === '0') {
+        jsBridge.pushWindow('/subcomment.html?type=2&id='
+          + self.authorID + '&cid=' + cid + '&rid=' + rid, {
+          title: '评论',
+        });
+      }
+    });
+    comment.on('closeSubComment', function() {
+      subCmt.to = '';
+    });
   }
   genDom() {
     let self = this;
@@ -45,6 +137,12 @@ class Author extends migi.Component {
             authorID={ self.authorID }
             homeDetail={ self.homeDetail }
             album={ self.album }/>
+      <SubCmt ref="subCmt"
+              originTo={ self.authorDetail.AuthorName }
+              subText="发送"
+              tipText="-${n}"
+              readOnly={ true }
+              placeholder={ '给' + self.authorDetail.AuthorName + '留个言吧' }/>
     </div>;
   }
   render() {
---
+++ src/author/Comments.jsx
@@ -0,0 +1,155 @@
+/**
+ * Created by army8735 on 2017/9/19.
+ */
+
+import util from '../common/util';
+import net from '../common/net';
+import Comment from '../component/comment/Comment.jsx';
+
+let take = 30;
+let skip = take;
+let sortType = 0;
+let myComment = 0;
+let currentCount = 0;
+let ajax;
+let loading;
+let loadEnd;
+let hidden;
+
+class Comments extends migi.Component {
+  constructor(...data) {
+    super(...data);
+    let self = this;
+    hidden = this.props.hidden;
+    self.authorID = self.props.authorID;
+    self.on(migi.Event.DOM, function() {
+      let $window = $(window);
+      $window.on('scroll', function() {
+        self.checkMore($window);
+      });
+      if(self.props.commentData.Size <= take) {
+        loadEnd = true;
+        self.ref.comment.message = '已经到底了';
+      }
+    });
+  }
+  @bind authorID
+  show() {
+    let self = this;
+    $(self.element).removeClass('fn-hide');
+    hidden = false;
+  }
+  hide() {
+    let self = this;
+    $(self.element).addClass('fn-hide');
+    hidden = true;
+  }
+  checkMore($window) {
+    if(hidden || loading || loadEnd) {
+      return;
+    }
+    let self = this;
+    let WIN_HEIGHT = $window.height();
+    let HEIGHT = $(document.body).height();
+    let bool;
+    bool = $window.scrollTop() + WIN_HEIGHT + 30 > HEIGHT;
+    if(bool) {
+      self.load();
+    }
+  }
+  load() {
+    let self = this;
+    let comment = self.ref.comment;
+    if(ajax) {
+      ajax.abort();
+    }
+    loading = true;
+    comment.message = '正在加载...';
+    ajax = net.postJSON('/api/author/commentList', { authorID: self.authorID, skip, take, sortType, myComment, currentCount }, function(res) {
+      if(res.success) {
+        let data = res.data;
+        skip += take;
+        if(data.data.length) {
+          comment.appendData(data.data);
+        }
+        if(skip >= data.Size) {
+          loadEnd = true;
+          comment.message = '已经到底了';
+        }
+        else {
+          comment.message = '';
+        }
+      }
+      else {
+        if(res.code === 1000) {
+          migi.eventBus.emit('NEED_LOGIN');
+        }
+        else {
+          alert(res.message || util.ERROR_MESSAGE);
+        }
+      }
+      loading = false;
+    }, function(res) {
+      alert(res.message || util.ERROR_MESSAGE);
+      loading = false;
+    });
+  }
+  switchType(e, vd) {
+    let $ul = $(vd.element);
+    $ul.toggleClass('alt');
+    $ul.find('li').toggleClass('cur');
+    let rel = $ul.find('.cur').attr('rel');
+    currentCount = 0;
+    sortType = rel;
+    skip = 0;
+    if(ajax) {
+      ajax.abort();
+    }
+    loadEnd = false;
+    loading = false;
+    this.ref.comment.clearData();
+    this.load();
+  }
+  switchType2(e, vd) {
+    let $ul = $(vd.element);
+    $ul.toggleClass('alt');
+    $ul.find('li').toggleClass('cur');
+    let rel = $ul.find('.cur').attr('rel');
+    currentCount = 0;
+    myComment = rel;
+    skip = 0;
+    if(ajax) {
+      ajax.abort();
+    }
+    loadEnd = false;
+    loading = false;
+    this.ref.comment.clearData();
+    this.load();
+  }
+  render() {
+    return <div class={ 'comments' + (this.props.hidden ? ' fn-hide' : '') }>
+      <div class="fn">
+        <ul class="type fn-clear" onClick={ { li: this.switchType2 } }>
+          <li class="cur" rel="0">全部评论<small>{ this.props.commentData.Count }</small></li>
+          {
+            this.props.isLogin
+              ? <li rel="1">我的</li>
+              : ''
+          }
+        </ul>
+        <ul class="type2 fn-clear" onClick={ { li: this.switchType } }>
+          <li class="cur" rel="0">最新</li>
+          <li rel="1">最热</li>
+        </ul>
+      </div>
+      <Comment ref="comment"
+               zanUrl="/api/author/likeComment"
+               subUrl="/api/author/subCommentList"
+               delUrl="/api/author/delComment"
+               data={ this.props.commentData.data }
+               message={ (!this.props.commentData.Size || this.props.commentData.Size > take) ? '' : '已经到底了' }/>
+    </div>;
+  }
+}
+
+export default Comments;
