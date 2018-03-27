4c692b3265b6e7e109a45ea444c02070f2cb3fe8
army8735 <army8735@qq.com>
2018-03-27 10:15:34 +0800

    remove download

--- src/works/Media.jsx
+++ src/works/Media.jsx
@@ -836,10 +836,6 @@ class Media extends migi.Component {
           <b class={ 'favor' + (this.isFavor ? ' favored' : '') }/>
           <span>{ this.isFavor ? '已收藏' : '收藏' }</span>
         </li>
-        <li class="dl" onClick={ this.clickDownload }>
-          <b class="download"/>
-          <span>下载</span>
-        </li>
         <li onClick={ this.clickShare }>
           <b class="share"/>
           <span>分享</span>
