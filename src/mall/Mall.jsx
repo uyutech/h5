160d222b612f35527ce2baadf3a7def47a6d4670
army8735 <army8735@qq.com>
2018-07-31 20:22:10 +0800

    取消马甲入住，商品说明

--- src/mall/Mall.jsx
+++ src/mall/Mall.jsx
@@ -52,14 +52,20 @@ class Mall extends migi.Component {
     }
     return res;
   }
+  desc(e, vd, tvd) {
+    let s = tvd.props.rel;
+    if(s) {
+      jsBridge.toast(s);
+    }
+  }
   render() {
     return <div class="mall">
       <ul class="type">
         <li>圈商城</li>
-        <li><a href="/mall_prize.html">新福利</a></li>
+        <li><a href="/mall_prize.html">我的福利</a></li>
         <li><a href="/mall_express.html">等待收货</a></li>
       </ul>
-      <div class="prod">
+      <div class="prod"onClick={ { 'img,.name': this.desc } }>
       {
         this.split(this.list || []).map(function(list) {
           return <div class="line fn-clear">
@@ -68,16 +74,20 @@ class Mall extends migi.Component {
                 let price = new BigNumber(item.price).times(item.discount).ceil();
                 if(item.discount < 1) {
                   return <div class="item">
-                    <img src={ $util.img(item.cover, 200, 200, 80) || '//zhuanquan.xin/img/blank.png' }/>
-                    <span class="name">{ item.name }</span>
+                    <img src={ $util.img(item.cover, 200, 200, 80) || '//zhuanquan.xin/img/blank.png' }
+                         rel={ item.describe }/>
+                    <span class="name"
+                          rel={ item.describe }>{ item.name }</span>
                     <span class="price discount">价格：{ item.price }<b class="icon"/></span>
                     <span class="price">折扣价：{ price }<b class="icon"/></span>
                     <button class="btn">即将上架</button>
                   </div>;
                 }
                 return <div class="item">
-                  <img src={ $util.img(item.cover, 200, 200, 80) || '//zhuanquan.xin/img/blank.png' }/>
-                  <span class="name">{ item.name }</span>
+                  <img src={ $util.img(item.cover, 200, 200, 80) || '//zhuanquan.xin/img/blank.png' }
+                       rel={ item.describe }/>
+                  <span class="name"
+                        rel={ item.describe }>{ item.name }</span>
                   <span class="price">价格：{ item.price }<b class="icon"/></span>
                   <button class="btn">即将上架</button>
                 </div>;
